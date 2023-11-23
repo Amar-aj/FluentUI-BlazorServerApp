1351d9c4cd37c9a91ebfda4f641168a23af2b055
Amar-aj <amar.aj.github@gmail.com>
2023-11-24 01:33:30 +0530

    added light and dark mode

--- FluentUI-BlazorApp/Program.cs
+++ FluentUI-BlazorApp/Program.cs
@@ -1,11 +1,13 @@
 using FluentUI_BlazorApp.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
